5d791b790ec57aed4258141febe98da0cce0315b
me-ljmr <lujamanandhar@Lujas-MacBook-Pro.local>
2016-04-21 17:55:29 -0400

    Shuffle Class Updated
    How to use this class:
    
    using Xzam.Utility;
    …
    
    QuestionCollection qlist = new QuestionCollection();
    qlist.AddQuestion(new Question( ... ));
    qlist.AddQuestion(new Question( ... ));
    Shuffle.Do (ref qlist);

--- Xzam/Utility/Shuffle.cs
+++ Xzam/Utility/Shuffle.cs
@@ -11,10 +11,17 @@ namespace Xzam.Utility
         /*
 			Created by Luja Manandhar 
 			Purpose: Shuffles the list of questions randomly sent as parameter for Do method
+
+			How to use: 
+			
+			QuestionCollection qlist = new QuestionCollection();
+			qlist.AddQuestion(new Question( ... ));
+			qlist.AddQuestion(new Question( ... ));
+			Shuffle.Do (ref qlist);
+
 		*/
         private static List<int> randomNums; 
-		public Shuffle(){ 
-		}
+		 
         public static void Do(ref List<Question> list)
         {
 			Random random = new Random ();
@@ -47,6 +54,40 @@ namespace Xzam.Utility
 			temp = null;
 			randomNums = null;
         }
+		public static void Do(ref QuestionCollection qcol)
+		{
+			Random random = new Random ();
+			randomNums = new List<int>(qcol.Count);
+			int counter = qcol.Count;
+
+			int num = 0;
+			int i;
+			for (i = 1; i <= counter; i++)
+			{
+
+				num = random.Next(1, counter);
+				if (randomNums.Count != 0)
+				{
+					while (randomNums.Contains(num))
+					{
+						num = random.Next(counter) + 1;
+					}
+				}
+				randomNums.Add(num);
+			}
+			// now proceed towards shuffling
+			Question temp;
+			i=0;
+			foreach (Question q in qcol) {
+				temp = qcol.QuestionList[i];
+				qcol.QuestionList[i] = qcol.QuestionList[randomNums[i] - 1];
+				qcol.QuestionList[randomNums[i] - 1] = temp;
+				i++;
+			}
+
+			temp = null;
+			randomNums = null;
+		}
 
     }
 }
